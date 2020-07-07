8dbec22cbb860efbb3c8f455db28d77cf641226d
Morgan Kitto <morgan@Morgans-MacBook.local>
2020-07-07 19:28:47 +0100

    UX features, added boundries to indicate anomolies, cleaned up code and comments

--- modelling.py
+++ modelling.py
@@ -1,40 +1,41 @@
 import csv
 from matplotlib import pyplot as plt
+
+
 data = []
-date, actual, expect, quantile, r_quantile = [],[],[],[],[]
+date, actual, expect, quantile, r_quantile = [], [], [], [], []
 
 
 ''' CHANGE NUMBER HERE TO CHANGE CSV'''
 
 
-number = 193
+number = 184
 
 '''
 
 '''
-with open(f"data/{number}.csv",newline="") as file:
+with open(f"data/{number}.csv", newline="") as file:
 
-    reader = csv.reader(file, delimiter = ",", quotechar = "|")
+    reader = csv.reader(file, delimiter=",", quotechar="|")
     x = 0
 
     for row in reader:
 
-        dt = [row[3],row[4],row[6],row[8]]
+        dt = [row[3], row[4], row[6], row[8]]
         data.append(dt)
 
-        if x > 1000:
-            break
-        x += 1
+        # if x > 1000:
+        #     break
+        # x += 1
 
 data = data[2:]
 
 
-
+# add data to lists of according columns
 for i in data:
     actual.append(float(i[0]))
     expect.append(float(i[1]))
     quantile.append(float(i[2]))
-    print(len(i))
 
     if i[3] == "":
 
@@ -42,13 +43,36 @@ for i in data:
     else:
         r_quantile.append(float(i[3]))
 
+# determines the beginning of rolling quantile (in case of change to calculation of rolling quantile time
 
-print(r_quantile)
+days = 0
+y = 0
+total_days = []
+for x in r_quantile:
+    total_days.append(y)
+    if x == 0:
+        days += 1
+    y += 1
 
-plt.plot(actual, label = "Actual")
-plt.plot(expect, label = "Expected")
-plt.plot(quantile, label = "Quantile")
-plt.plot(r_quantile, label = "Rolling Quantile")
+
+# plot graph
+
+plt.plot(actual, label="Actual")
+plt.plot(expect, label="Expected")
+
+# plot quantiles
+plt.plot(quantile, label="Quantile")
+plt.plot(total_days[days:], r_quantile[days:], label="Rolling Quantile")
+
+# boundries for quantiles
+plt.hlines(0.0005, 0, len(total_days), colors="#DA3E1C", linestyles="--", alpha=0.7, label="Quantile Threshold")
+plt.hlines(0.9995, 0, len(total_days), colors="#DA3E1C", linestyles="--", alpha=0.7)
+
+# Boundries for rolling quantiles
+plt.hlines(0.7, 0, len(total_days), colors="#000000", linestyles="--", alpha=0.7, label="Rolling Quantile Threshold")
+plt.hlines(0.3, 0, len(total_days), colors="#000000", linestyles="--", alpha=0.7)
+
+# format graph
 plt.xlabel("Time (Indexed)")
 plt.ylabel("")
 plt.title(f"ML Model Analysis ({number})")
