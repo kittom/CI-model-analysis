84380ccd15fc029d62f7c6462b5f2f7d84b4d55c
Morgan Kitto <morgan@Morgans-MacBook.local>
2020-07-07 17:12:03 +0100

    UX features and rolling quantile

--- modelling.py
+++ modelling.py
@@ -1,19 +1,29 @@
 import csv
 from matplotlib import pyplot as plt
 data = []
-date, actual, expect, quantile = [],[],[],[]
+date, actual, expect, quantile, r_quantile = [],[],[],[],[]
 
-with open("data/193.csv",newline="") as file:
+
+''' CHANGE NUMBER HERE TO CHANGE CSV'''
+
+
+number = 193
+
+'''
+
+'''
+with open(f"data/{number}.csv",newline="") as file:
 
     reader = csv.reader(file, delimiter = ",", quotechar = "|")
     x = 0
 
     for row in reader:
 
-        dt = [row[3],row[4],row[6]]
+        dt = [row[3],row[4],row[6],row[8]]
         data.append(dt)
 
-
+        if x > 1000:
+            break
         x += 1
 
 data = data[2:]
@@ -24,13 +34,25 @@ for i in data:
     actual.append(float(i[0]))
     expect.append(float(i[1]))
     quantile.append(float(i[2]))
-    # date.append(i[3])
+    print(len(i))
+
+    if i[3] == "":
+
+        r_quantile.append(0)
+    else:
+        r_quantile.append(float(i[3]))
 
 
-print(actual)
+print(r_quantile)
 
-plt.plot(actual), plt.plot(expect), plt.plot(quantile)
-plt.xlabel("date")
-plt.ylabel("actual")
-plt.title("ML Model Analysis")
+plt.plot(actual, label = "Actual")
+plt.plot(expect, label = "Expected")
+plt.plot(quantile, label = "Quantile")
+plt.plot(r_quantile, label = "Rolling Quantile")
+plt.xlabel("Time (Indexed)")
+plt.ylabel("")
+plt.title(f"ML Model Analysis ({number})")
+plt.legend()
+plt.grid(True)
+
 plt.show()
